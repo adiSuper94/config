601423eccb32e7e49248b0a9e6f541f1d4b20551
Aditya Subramanian <aditarun2008@gmail.com>
2023-02-06 20:22:36 -0500

    tat clean up

--- tmux/tat.sh
+++ tmux/tat.sh
@@ -2,20 +2,18 @@
 not_in_tmux() {
   [ -z "$TMUX" ]
 }
+session_exists(){
+  tmux has-session -t $1
+}
 
 # switch to existing session
 switch_or_create_session(){
   selected_session=$( (tmux list-sessions | sed "s/:.*//"; echo "--new-session--") | fzf)
   if [ "$selected_session" = "--new-session--" ]; then
-    #echo "enter name of new session: "
+    echo "enter name of new session: "
     read selected_session
-    if [ -z session_exists session_name ]; then
-      tmux new-session -d -s "$selected_session"
-      #echo "created detached tmux session : $selected_session"
-    else
-      echo "session name already exits"
-    fi
   fi
+  
   if not_in_tmux; then
     #echo "attach n  -d -t "
     tmux new-session -As "$selected_session"
@@ -30,7 +28,7 @@ switch_session() {
   # for first session use defaut name "noname"
   if [ -z "$(tmux list-sessions)" ]; then
     selected_session="noname"
-    tmux new-session -s "$selected_session"
+    tmux new-session -As "$selected_session"
   else
     switch_or_create_session
   fi
