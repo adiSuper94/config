ea510f4ea707221e69ad716f02660214730c18d7
Aditya Subramanian <aditarun2008@gmail.com>
2024-04-16 10:08:08 -0400

    intstal tmux, fzf, nvim, j, exa, rg, bat, bat-extra, htop

--- setup.sh
+++ setup.sh
@@ -25,20 +25,6 @@ get_os() {
   fi
 }
 
-## ensure zsh is installed
-install_zsh() {
-  if ! command -v zsh &> /dev/null; then
-    if [[ $os == "MacOS" ]]; then
-      brew install zsh
-    else
-      sudo apt-get update
-      sudo apt-get install zsh
-    fi
-  else
-    echo "zsh already installed"
-  fi
-}
-
 ## check if default shell is zsh
 default_shell_is_zsh() {
   if [[ $SHELL != $(which zsh) ]]; then
@@ -46,76 +32,118 @@ default_shell_is_zsh() {
   fi
 }
 
-get_os
-# install homebrew if MacOS
-if [[ $os == "MacOS" ]]; then
-  install_homebrew
-fi
-install_zsh
-default_shell_is_zsh
 
-# check if  htop, tmux, fzf, ripgrep, bat, bat-extra, exa, autojump, bat-extras is installed, if not then install
-if ! command -v htop &> /dev/null; then
-  if [[ $os == "MacOS" ]]; then
-    brew install htop
-  else
+ubuntu_setup(){
+  sudo apt-get update
+  if [ ! -d "$HOME/nutter-tools" ]; then
+    mkdir -p $HOME/nutter-tools/bin
+  fi
+  # check if  htop, tmux, fzf, ripgrep, bat, bat-extra, exa, autojump, is installed, if not then install
+  if ! command -v curl &> /dev/null; then
+    sudo apt-get install curl
+  fi
+
+  if ! command -v nvim &> /dev/null; then
+    curl -o $HOME/nutter-tools/nvim-linux64.tar.gz -L https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+    tar -xvf $HOME/nutter-tools/nvim-linux64.tar.gz --directory $HOME/nutter-tools/
+    ln -s $HOME/nutter-tools/nvim-linux64/bin/nvim $HOME/nutter-tools/bin/nvim
+    rm $HOME/nutter-tools/nvim-linux64.tar.gz
+  fi
+
+  if ! command -v zsh &> /dev/null; then
+    sudo apt-get install zsh
+  fi
+
+  default_shell_is_zsh
+
+  if ! command -v htop &> /dev/null; then
     sudo apt-get install htop
   fi
-fi
 
-if ! command -v tmux &> /dev/null; then
-  if [[ $os == "MacOS" ]]; then
-    brew install tmux
-  else
+  if ! command -v tmux &> /dev/null; then
     sudo apt-get install tmux
   fi
-fi
 
-if ! command -v fzf &> /dev/null; then
-  if [[ $os == "MacOS" ]]; then
-    brew install fzf
-  else
+  if ! command -v fzf &> /dev/null; then
     sudo apt-get install fzf
   fi
-fi
 
-if ! command -v rg &> /dev/null; then
-  if [[ $os == "MacOS" ]]; then
-    brew install ripgrep
-  else
+  if ! command -v rg &> /dev/null; then
     sudo apt-get install ripgrep
   fi
-fi
 
-if ! command -v bat &> /dev/null; then
-  if [[ $os == "MacOS" ]]; then
-    brew install bat
-  else
+  if ! command -v bat &> /dev/null; then
     sudo apt-get install bat
   fi
-fi
 
-if ! command -v exa &> /dev/null; then
-  if [[ $os == "MacOS" ]]; then
-    brew install exa
-  else
+  if ! command -v bat-extras &> /dev/null; then
+    git clone git@github.com:eth-p/bat-extras.git $HOME/nutter-tools/bat-extras
+    ln -s $HOME/nutter-tools/bat-extras/bin/* $HOME/nutter-tools/bin
+  fi
+
+  if ! command -v exa &> /dev/null; then
     sudo apt-get install exa
   fi
-fi
 
-if ! command -v autojump &> /dev/null; then
-  if [[ $os == "MacOS" ]]; then
-    brew install autojump
-  else
+  if ! command -v autojump &> /dev/null; then
     sudo apt-get install autojump
   fi
-fi
+}
 
-if ! command -v bat-extras &> /dev/null; then
-  if [[ $os == "MacOS" ]]; then
-    brew install bat-extras
-  else
-    # install bat-extras from source
+mac_setup(){
+  install_homebrew
+  # check if  nvim, htop, tmux, fzf, ripgrep, bat, bat-extra, exa, autojump, is installed, if not then install
+
+  if ! command -v nvim &> /dev/null; then
+    brew install neovim
   fi
-fi
+
+  if ! command -v zsh &> /dev/null; then
+    brew install zsh
+  fi
+
+  default_shell_is_zsh
+
+  if ! command -v htop &> /dev/null; then
+    brew install htop
+  fi
+
+  if ! command -v tmux &> /dev/null; then
+    brew install tmux
+  fi
+
+  if ! command -v fzf &> /dev/null; then
+    brew install fzf
+  fi
+
+  if ! command -v rg &> /dev/null; then
+    brew install ripgrep
+  fi
+
+  if ! command -v bat &> /dev/null; then
+    brew install bat
+  fi
+
+  if ! command -v bat-extras &> /dev/null; then
+    brew install bat-extras
+  fi
+
+  if ! command -v exa &> /dev/null; then
+    brew install exa
+  fi
+
+  if ! command -v autojump &> /dev/null; then
+    brew install autojump
+  fi
+}
+
+setup() {
+  get_os
+  case $os in
+    Ubuntu|Debian) ubuntu_setup ;;
+    MacOS) mac_setup ;;
+  esac
+}
+
+setup
 
