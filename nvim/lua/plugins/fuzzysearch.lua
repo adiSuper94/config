d90cb22f8e54e3a58cc79cf44140664fe775c55a
Aditya Subramanian <aditarun2008@gmail.com>
2025-12-09 12:45:06 -0500

    nvim: fff

--- nvim/lua/plugins/fuzzysearch.lua
+++ nvim/lua/plugins/fuzzysearch.lua
@@ -1,86 +1,121 @@
-local searcher = "raw-dog" --- "telescope" | "raw-dog"
-if searcher == "raw-dog" then
-  vim.opt.grepprg = "rg --vimgrep --smart-case"
-  vim.keymap.set("n", "<leader>/", function()
-    local pattern = vim.fn.input("rg: ")
-    if pattern ~= "" then
-      vim.cmd('silent grep! "' .. pattern ..'"')
-      vim.cmd("copen")
-    end
-  end, { desc = "raw-dog: Live grep" })
+local searcher = "fff" --- "telescope" | "raw-dog" | "fff"
 
-  function Fd(file_pattern, _)
-    -- if first char is * then fuzzy search
-    if file_pattern:sub(1, 1) == "*" then
-      file_pattern = file_pattern:gsub(".", ".*%0") .. ".*"
-    end
-    local cmd = 'fd  --color=never --full-path --type file --hidden --exclude=".git" "' .. file_pattern .. '"'
-    local result = vim.fn.systemlist(cmd)
-    return result
+vim.opt.grepprg = "rg --vimgrep --smart-case"
+function Rg()
+  local pattern = vim.fn.input("rg: ")
+  if pattern ~= "" then
+    vim.cmd('silent grep! "' .. pattern .. '"')
+    vim.cmd("copen")
   end
-  vim.opt.findfunc = "v:lua.Fd"
+end
+
+function Fd(file_pattern, _)
+  -- if first char is * then fuzzy search
+  if file_pattern:sub(1, 1) == "*" then
+    file_pattern = file_pattern:gsub(".", ".*%0") .. ".*"
+  end
+  local cmd = 'fd  --color=never --full-path --type file --hidden --exclude=".git" "' .. file_pattern .. '"'
+  local result = vim.fn.systemlist(cmd)
+  return result
+end
+
+vim.opt.findfunc = "v:lua.Fd"
+vim.api.nvim_create_user_command('Rg', Rg, {})
+
+if searcher == "raw-dog" then
   vim.keymap.set("n", "<C-p>", ":find ", { desc = "raw-dog: Project Files" })
+  vim.keymap.set("n", "<leader>/", ":Rg<CR>", { desc = "raw-dog: Live grep" })
   return {}
 end
 
-return {
-  {
-    "nvim-telescope/telescope.nvim",
-    enabled = searcher == "telescope",
-    tag = "0.1.8",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-      "nvim-telescope/telescope-ui-select.nvim",
-    },
-    config = function()
-      local default_theme = "dropdown"
-      require("telescope").setup({
-        pickers = {
-          find_files = {
-            theme = default_theme,
-            previewer = false,
+if searcher == "telescope" then
+  return {
+    {
+      "nvim-telescope/telescope.nvim",
+      enabled = searcher == "telescope",
+      tag = "v0.2.0",
+      dependencies = {
+        "nvim-lua/plenary.nvim",
+        { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+        "nvim-telescope/telescope-ui-select.nvim",
+      },
+      config = function()
+        local default_theme = "dropdown"
+        require("telescope").setup({
+          pickers = {
+            find_files = {
+              theme = default_theme,
+              previewer = false,
+            },
+            git_files = {
+              theme = default_theme,
+              previewer = false,
+            },
+            builtin = {
+              theme = default_theme,
+              previewer = false,
+            },
+            current_buffer_fuzzy_find = {
+              previewer = false,
+            },
           },
-          git_files = {
-            theme = default_theme,
-            previewer = false,
+          extensions = {
+            ["ui-select"] = {
+              require("telescope.themes").get_ivy(),
+            },
           },
-          builtin = {
-            theme = default_theme,
-            previewer = false,
-          },
-          current_buffer_fuzzy_find = {
-            previewer = false,
-          },
-        },
-        extensions = {
-          ["ui-select"] = {
-            require("telescope.themes").get_ivy(),
-          },
-        },
-      })
-      local utils = require("telescope.utils")
-      local builtin = require("telescope.builtin")
-      local project_files = function()
-        local _, ret, _ = utils.get_os_command_output({ "git", "rev-parse", "--is-inside-work-tree" })
-        if ret == 0 then
-          builtin.git_files()
-        else
-          builtin.find_files()
+        })
+        local utils = require("telescope.utils")
+        local builtin = require("telescope.builtin")
+        local project_files = function()
+          local _, ret, _ = utils.get_os_command_output({ "git", "rev-parse", "--is-inside-work-tree" })
+          if ret == 0 then
+            builtin.git_files()
+          else
+            builtin.find_files()
+          end
         end
-      end
-      vim.keymap.set("n", "<C-p>", project_files, { desc = "Project Files" })
+        vim.keymap.set("n", "<C-p>", project_files, { desc = "Project Files" })
 
-      vim.keymap.set("n", "<leader>tj", builtin.builtin, { desc = "Telescope builtins" })
-      vim.keymap.set("n", "<leader>tf", builtin.find_files, { desc = "Telescope find files" })
-      vim.keymap.set("n", "<leader>/", builtin.live_grep, { desc = "Telescope live grep" })
-      vim.keymap.set("n", "<leader>th", builtin.help_tags, { desc = "Telescope help tags" })
-      vim.keymap.set("n", "<leader>c", function()
-        builtin.git_files({ cwd = "~/.config/nvim/" })
-      end, { desc = "Open nvim init.lua" })
+        vim.keymap.set("n", "<leader>tj", builtin.builtin, { desc = "Telescope builtins" })
+        vim.keymap.set("n", "<leader>tf", builtin.find_files, { desc = "Telescope find files" })
+        vim.keymap.set("n", "<leader>/", builtin.live_grep, { desc = "Telescope live grep" })
+        vim.keymap.set("n", "<leader>th", builtin.help_tags, { desc = "Telescope help tags" })
+        vim.keymap.set("n", "<leader>c", function()
+          builtin.git_files({ cwd = "~/.config/nvim/" })
+        end, { desc = "Open nvim init.lua" })
 
-      require("telescope").load_extension("fzf")
-      require("telescope").load_extension("ui-select")
+        require("telescope").load_extension("fzf")
+        require("telescope").load_extension("ui-select")
+      end,
+    },
+  }
+end
+
+if searcher == "fff" then
+  vim.keymap.set("n", "<leader>/", ":Rg<CR>", { desc = "raw-dog: Live grep" })
+  return
+  {
+    'dmtrKovalenko/fff.nvim',
+    build = function()
+      -- this will download prebuild binary or try to use existing rustup toolchain to build from source
+      -- (if you are using lazy you can use gb for rebuilding a plugin if needed)
+      require("fff.download").download_or_build_binary()
     end,
-  },
-}
+    opts = {                -- (optional)
+      debug = {
+        enabled = true,     -- we expect your collaboration at least during the beta
+        show_scores = true, -- to help us optimize the scoring system, feel free to share your scores!
+      },
+    },
+    -- No need to lazy-load with lazy.nvim, this plugin initializes itself lazily.
+    lazy = false,
+    keys = {
+      {
+        "<C-p>",
+        function() require('fff').find_files() end,
+        desc = 'FFFind files',
+      }
+    }
+  }
+end
--- nvim/lua/plugins/misc.lua
+++ nvim/lua/plugins/misc.lua
@@ -2,7 +2,7 @@
 return {
   {
     "github/copilot.vim",
-    enabled = false,
+    enabled = true,
     config = function()
       vim.api.nvim_set_keymap("i", "<C-k>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
       vim.g.copilot_no_tab_map = true
