a9f71a3b962aeed779406e73e59481cc69c020a0
Aditya Subramanian <aditarun2008@gmail.com>
2026-04-11 01:02:52 -0400

    nvim: formatprg

--- nvim/lua/plugins/format.lua
+++ nvim/lua/plugins/format.lua
@@ -1,8 +1,4 @@
-vim.pack.add({
-  "https://github.com/stevearc/conform.nvim",
-})
-
-local function get_formatter()
+local function get_formatprg()
   local js_formatter = { "oxfmt", "deno_fmt", "prettier" }
   local formatters_by_ft = {
     javascript = js_formatter,
@@ -18,63 +14,66 @@ local function get_formatter()
   local formatter_config = {
     oxfmt = { ".oxfmtrc.json" },
     deno_fmt = { "deno.json", "deno.jsonc" },
-    prettier = { "prettier.config.js", ".prettierrc", ".prettierrc.json", ".prettierrc.yaml", ".prettierrc.yml", "package.json" },
-    stylua = { "stylua.toml" },
+    prettier = {
+      "prettier.config.js",
+      ".prettierrc",
+      ".prettierrc.json",
+      ".prettierrc.yaml",
+      ".prettierrc.yml",
+    },
+    stylua = { "stylua.toml", ".luarc.json" },
     ruff_format = { "pyproject.toml" },
+  }
+  local formatprgs = {
+    oxfmt = "oxfmt --stdin-filepath=%",
+    deno_fmt = "deno fmt -",
+    prettier = "prettier",
+    stylua = "stylua -",
+    ruff_format = "ruff format -",
   }
   local ft = vim.bo.filetype
   local formatters = formatters_by_ft[ft] or {}
   for _, formatter in ipairs(formatters) do
     local config_files = formatter_config[formatter] or {}
     for _, config_file in ipairs(config_files) do
-      local path = vim.api.nvim_buf_get_name(0);
-      local stop = vim.fn.getcwd() .. "../"
-      if vim.fs.find(config_file, { path, stop, upward = true })[1] then
-        return formatter
+      local path = vim.fs.dirname(vim.api.nvim_buf_get_name(0))
+      local stop = vim.fn.getcwd() .. "/.."
+      if vim.fs.find(config_file, { path = path, stop = stop, upward = true, limit = 1 })[1] then
+        return formatprgs[formatter]
       end
     end
   end
   return nil
 end
 
-local formatter_cache = {}
-local cache_size = 0;
-function Format()
-  local filepath = vim.api.nvim_buf_get_name(0);
-  local formatter = nil;
-  if formatter_cache[filepath] then
-    formatter = formatter_cache[filepath]
+function Fmt()
+  local formatprg = vim.bo.formatprg ~= "" and vim.bo.formatprg or nil
+  if formatprg and formatprg ~= "" then
+    local v = vim.fn.winsaveview()
+    vim.cmd.normal({ "gggqG", bang = true, mods = { silent = true } })
+    vim.fn.winrestview(v);
   else
-    formatter = get_formatter()
-    if formatter and cache_size >= 20 then
-      formatter_cache = {}
-      cache_size = 0
-    elseif formatter then
-      cache_size = cache_size + 1
-    end
-  end
-  if formatter then
-    require("conform").format({ async = true, formatters = { formatter } })
-    formatter_cache[filepath] = formatter
-  else
-    require("conform").format({ async = true })
+    vim.lsp.buf.format({ async = true })
   end
 end
 
-require("conform").setup({
-  formatters_by_ft = {
-    javascript = { "prettierd", "prettier", stop_after_first = true },
-    typescript = { "prettierd", "prettier", stop_after_first = true },
-    javascriptreact = { "prettierd", "prettier", stop_after_first = true },
-    typescriptreact = { "prettierd", "prettier", stop_after_first = true },
-    json = { "prettierd", "prettier", "oxfmt", stop_after_first = true },
-    jsonc = { "prettierd", "prettier", "oxfmt", stop_after_first = true },
-    css = { "prettierd", "prettier", "oxfmt", stop_after_first = true },
-    lua = { "stylua" },
-    python = { "ruff_format" },
-  },
-
-  default_format_opts = { lsp_format = "fallback" },
+local formatprg_ag = vim.api.nvim_create_augroup("set_formatprg", { clear = true })
+vim.api.nvim_create_autocmd("BufNew", {
+  pattern = "*",
+  group = formatprg_ag,
+  callback = function()
+    local formatprg = get_formatprg()
+    if formatprg then
+      vim.bo.formatprg = formatprg
+    end
+  end,
 })
 
-vim.keymap.set("n", "<leader>f", Format, { desc = "Format file" })
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*",
+  callback = function()
+    Fmt()
+  end,
+})
+
+vim.keymap.set("n", "<leader>f", Fmt, { desc = "Format file" })
